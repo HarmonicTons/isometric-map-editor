3157d6b9703c2e93c55ca1972fab82e571963a79
HarmonicTons <roncin.thomas.l.l@gmail.com>
2026-07-08 16:27:42 +0200

    test: add more snapshot tests

--- src/app/screens/main/__tests__/mapRender.test.ts
+++ src/app/screens/main/__tests__/mapRender.test.ts
@@ -27,7 +27,7 @@ const testsDir = path.dirname(fileURLToPath(import.meta.url));
 const mapsDir = path.resolve(testsDir, "../../../../../public/maps");
 const snapshotsDir = path.join(testsDir, "__image_snapshots__");
 
-const SNAPSHOT_MAPS = ["deti-plains", "koring-wood"];
+const SNAPSHOT_MAPS = ["single", "checkboard", "rock-cubes", "dirt-grass",  "rock-grass", "wall-dirt", "deti-plains", "koring-wood", ];
 
 describe("map render snapshots", () => {
   // Map's constructor is chatty
